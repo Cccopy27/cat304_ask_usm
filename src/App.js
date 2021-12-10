dd4268809f4a660a95d8d41438c6364d5fb3887d
Cccopy27 <wenhao0627@student.usm.my>
2021-12-10 23:09:50 +0800

    added navigation to question pages

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Navbar from './components/Navbar';
 import {Routes,Route, BrowserRouter} from "react-router-dom";
 import Sidebar from './components/Sidebar';
+import QuestionDashboard from './pages/question/QuestionDashboard';
 
 function App() {
   return (
@@ -11,7 +12,7 @@ function App() {
       <Sidebar/>
       <div className='content'>
         <Routes>
-          
+          <Route path="/question" element={<QuestionDashboard/>}/>
         </Routes>
       </div>
       
