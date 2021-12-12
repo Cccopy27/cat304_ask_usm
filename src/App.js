0e3888c6382aa67eafb0d7d8035dad511c71ae1e
Cccopy27 <wenhao0627@student.usm.my>
2021-12-12 15:30:23 +0800

    added addquestion path to route for navigation

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Navbar from './components/Navbar';
 import {Routes,Route, BrowserRouter} from "react-router-dom";
 import Sidebar from './components/Sidebar';
 import QuestionDashboard from './pages/question/QuestionDashboard';
+import AddQuestion from './pages/question/AddQuestion';
 
 function App() {
   return (
@@ -13,6 +14,8 @@ function App() {
       <div className='content'>
         <Routes>
           <Route path="/question" element={<QuestionDashboard/>}/>
+          <Route path="/addquestion" element={<AddQuestion/>}/>
+
         </Routes>
       </div>
       
