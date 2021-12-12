ab3f32c0b4215f8c89573dbd74882ad2042f02c6
Cccopy27 <wenhao0627@student.usm.my>
2021-12-12 16:07:06 +0800

    addquestion form added upload image function

--- src/pages/question/AddQuestion.js
+++ src/pages/question/AddQuestion.js
@@ -1,7 +1,60 @@
+import { useState } from "react"
+
 export default function AddQuestion() {
+    const [title, settitle] = useState("");
+    const [des, setdes] = useState(""); 
+    const [tag, settag] = useState([]);
+    const [image, setimage] = useState([]);
+
+    // when user submit the form
+    const handleSubmit=(e)=>{
+        e.preventDefault();
+        const object={
+            question_title: title,
+            question_description: des,
+            question_tag: tag,
+            question_image: image
+        }
+        console.log(object);
+    }
     return (
-        <div>
-            hi
+        <div className="add-question-form">
+            <h2>Add new question</h2>
+            <form onSubmit={handleSubmit}>
+                <label>
+                    <span>Question title:</span>
+                    <input
+                    required
+                    type="text"
+                    onChange={e => {settitle(e.target.value)}}
+                    value={title}
+                    />
+                </label>
+
+                <label>
+                    <span>Question description:</span>
+                    <textarea
+                    required
+                    onChange={e => {setdes(e.target.value)}}
+                    value={des}
+                    />
+                </label>
+
+                {/* add tag here  */}
+
+                <label>
+                    <span>Image:</span>
+                    <input
+                    type="file"
+                    onChange={e => {setimage(e.target.value)}}
+                    value={image}
+                    multiple accept="image/*"
+                    />
+                </label>
+
+                <button>Add question</button>
+                
+            </form>
         </div>
     )
 }
