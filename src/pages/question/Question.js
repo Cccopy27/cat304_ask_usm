26f17b67c89a40f8dc65e748346ad0586e682fb4
Cccopy27 <wenhao0627@student.usm.my>
2021-12-28 23:17:23 +0800

    format added time

--- src/pages/question/Question.js
+++ src/pages/question/Question.js
@@ -1,5 +1,7 @@
 import "./Question.css";
 import {useParams} from "react-router-dom";
+import {useDocument} from "../../hooks/useDocument";
+import formatDistanceToNow from "date-fns/formatDistanceToNow";
 
 export default function Question() {
     const {id} = useParams();
@@ -10,15 +12,15 @@ export default function Question() {
     if(!document){
         return <div>Loading...</div>
     }
-
+    console.log(document);
     return (
         <div>
             <div className="question-details">
                 <h2>{document.question_title}</h2>
                 <p>{document.question_description}</p>
-                <p>{document.question_tag}</p>
-                <p>{document.added_at}</p>
-                <p>{document.created_by}</p>
+                <p>tags:{document.question_tag}</p>
+                <p>added {formatDistanceToNow(document.added_at.toDate(),{addSuffix:true})}</p>
+                <p>created_by: {document.created_by}</p>
 
             </div>
         </div>
