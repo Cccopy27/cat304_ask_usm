8afd6c3646b18ce4ea6aae27a8a582822091e769
Cccopy27 <wenhao0627@student.usm.my>
2021-12-28 23:10:24 +0800

    add details in question page single

--- src/pages/question/Question.js
+++ src/pages/question/Question.js
@@ -3,10 +3,24 @@ import {useParams} from "react-router-dom";
 
 export default function Question() {
     const {id} = useParams();
-    
+    const {error, document} = useDocument("questions",id);
+    if(error){
+        return <div>{error}</div>
+    }
+    if(!document){
+        return <div>Loading...</div>
+    }
+
     return (
         <div>
-            
+            <div className="question-details">
+                <h2>{document.question_title}</h2>
+                <p>{document.question_description}</p>
+                <p>{document.question_tag}</p>
+                <p>{document.added_at}</p>
+                <p>{document.created_by}</p>
+
+            </div>
         </div>
     )
 }
