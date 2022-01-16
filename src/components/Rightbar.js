2ab0b92b7bbcc0a8680ce3e7b40fad2b7d5b17ba
Cccopy27 <wenhao0627@student.usm.my>
2022-01-16 14:25:55 +0800

    added rigtbar content, bookmark function and my question function ui

--- src/components/Rightbar.js
+++ src/components/Rightbar.js
@@ -1,13 +1,49 @@
 import styles from "./Rightbar.module.css";
+import { NavLink } from "react-router-dom";
 
 export default function RightBar() {
     return (
         <div className={styles.rightbar_container}>
             
             <div className={styles.rightbar_content}>
-                <div className={styles.rightbar_content1}>random content 1</div>
-                <div className={styles.rightbar_content2}>random content 2</div>
-                <div className={styles.rightbar_content3}>random content 3</div>
+                <div className={styles.my_question}>
+                    <button className={styles.my_question_btn}>Go to My Question</button>
+                </div>
+
+                <div className={styles.bookmark_container}>
+                    <div className={styles.bookmark_title}>
+                        <span className={styles.bookmark_word}>Bookmark</span>
+                        <button className={styles.bookmark_edit}>Edit</button>
+                    </div>
+                    <div className={styles.bookmark_content}>
+
+                        <div className={styles.bookmark_tag}>
+                            <span className={styles.bookmark_tag_title}>Tag</span>
+                            <ul className={styles.bookmark_ul}>
+                                <li>tag 1</li>
+                                <li>tag 2</li>
+                                <li>tag 3</li>
+                                <li>tag 4</li>
+                                <li>tag 5</li>
+                            </ul>
+                            <span className={styles.viewmore}> + View more</span>
+                        </div>
+                        <div className={styles.bookmark_tag}>
+                            <span className={styles.bookmark_tag_title}>User</span>
+                            <ul className={styles.bookmark_ul}>
+                                <li>User 1</li>
+                                <li>User 2</li>
+                                <li>User 3</li>
+                                <li>User 4</li>
+                                <li>User 5</li>
+                            </ul>
+                            <span className={styles.viewmore}> + View more</span>
+
+                        </div>
+                    </div>
+                    
+                </div>
+                
 
             </div>
             
