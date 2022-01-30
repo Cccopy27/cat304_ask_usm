c8d8f822527c67b4ceb462a12b36ac6b468dd488
Cccopy27 <wenhao0627@student.usm.my>
2022-01-30 17:37:09 +0800

    fix bug

--- src/pages/post/EditPost.js
+++ src/pages/post/EditPost.js
@@ -178,7 +178,7 @@ export default function EditPost({document,editMode,setEditMode,displayName}) {
                     if(image.length !== 0){
                         document.post_image_name.forEach(image_name=>{
                             // Create a reference to the file to delete
-                            const desertRef = ref(storage, `posts/${document.id}/${image_name}`);
+                            const desertRef = ref(storage, `post/${document.id}/${image_name}`);
                             // Delete the file
                             deleteObject(desertRef).then(() => {
                                 // File deleted successfully
